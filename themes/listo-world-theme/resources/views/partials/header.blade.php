7ddb5499a8a994e8ab8cb32e2e49593b6b9ca492
gabzon <gab.zambrano@gmail.com>
2019-07-07 19:36:06 +0200

    navbar

--- themes/listo-world-theme/resources/views/partials/header.blade.php
+++ themes/listo-world-theme/resources/views/partials/header.blade.php
@@ -1,7 +1,10 @@
-<header class="banner">
+<header class="banner navbar navbar-expand-lg navbar-light bg-light">
   <div class="container">
-    <a class="brand" href="{{ home_url('/') }}">{{ get_bloginfo('name', 'display') }}</a>
-    <nav class="nav-primary">
+    <a class="brand navbar-brand" href="{{ home_url('/') }}">{{ get_bloginfo('name', 'display') }}</a>
+    <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
+    <span class="navbar-toggler-icon"></span>
+  </button>
+    <nav class="nav-primary collapse navbar-collapse" id="main-navbar">
       @if (has_nav_menu('primary_navigation'))
         {!! wp_nav_menu(['theme_location' => 'primary_navigation', 'menu_class' => 'nav']) !!}
       @endif
